97fff239877dae27333f922f38f6e872107e9d78
agleis <amg295@cornell.edu>
2016-12-04 22:49:47 -0500

    Adam mli comments

--- ai_controller.mli
+++ ai_controller.mli
@@ -1,7 +1,16 @@
 open Types
 
-(* [guess_turn p_state pool data] determines which path the AI should take
- * and updates the game and player states. *)
+(* [guess_turn p_state pool data] returns the card that this AI wishes to play
+ * for the current turn, where [pool] holds the cards already played, [data]
+ * holds data about the round, and [p_state] holds information about this AI.
+ * Precondition: [p_state.hand] is non-empty.
+ * Postcondition: Returns a card found in the list [p_state.hand]. *)
 val guess_turn : player_state -> card list -> stored_data -> card
 
+(* [pass_cards p_state] chooses three cards from the list [p_state.hand] to
+ * pass to the next player during the Passing phase of the game. The actual
+ * cards chosen will be different based on [p_state.ai_level].
+ * Precondition: [p_state.hand] is non-empty.
+ * Postcondition: Returns a list of cards of size three, each of which can
+ * be found in [p_state.hand]. *)
 val pass_cards : player_state -> card list
